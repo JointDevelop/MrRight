6e9266eb71ad6e54593b7c22eed5ca76f7815dca
lyumin <2440151307@qq.com>
2020-11-09 09:58:32 +0800

    1、添加 index 首页 2、添加 user 模块，并定义接口和path 3、修改 settings

---
+++ user/views.py
@@ -0,0 +1,27 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def fetch_vcode(request):
+    return JsonResponse({})
+
+
+def submit_vcode(request):
+    return JsonResponse({})
+
+
+def show_profile(request):
+    return JsonResponse({})
+
+
+def update_profile(request):
+    return JsonResponse({})
+
+
+def get_qn_token(request):
+    return JsonResponse({})
+
+
+def qn_callback(request):
+    return JsonResponse({})
