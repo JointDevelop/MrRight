f9de7fc32ca526cb006bae64ae02e32ac1ccee77
lyumin <2440151307@qq.com>
2021-03-16 14:47:01 +0800

    1、重构推荐好友的函数 2、修改 like、superlike 函数：操作后将对方 id 从自己的推荐队列中删除

--- social/tools.py
+++ social/tools.py
@@ -9,23 +9,41 @@ from social.models import Friend
 from libs.cache import rds
 
 
-def recommand_users(uid):
-    ''' Algorithm of recommanding users'''
+def recommand_from_queue(uid):
+    ''' recommand from priority queue '''
+    myKey = RCMD_QUE % uid
+    uids_list = [int(sid) for sid in rds.lrange(myKey, 0, 19)]
+    return User.objects.filter(id__in=uids_list)
+
+
+def recommand_from_db(uid,num):
+    ''' recomand from database '''
     target_profile = Profile.objects.get(id=uid)
+    # calculate birthday
     today = datetime.date.today()
     earliest_birthday = today - datetime.timedelta(target_profile.max_dating_age * 365)
     latest_birthday = today + datetime.timedelta(target_profile.min_dating_age * 365)
+    # find the users who matched the basic condition.
     target_users = User.objects.filter(
         gender=target_profile.dating_gender,
         location=target_profile.dating_location,
         birthday__range=[earliest_birthday, latest_birthday]
         # TODO: others like 'min_distance' and 'max_distance' wait to be deal with. We pass them temperarily.
     )
-
     # Exclude the users who had already been swiped 'like'/'superlike'/'dislike' by login-User.
     already_swape_list = Swiped.objects.filter(uid=uid).values_list('sid', flat=True)
     target_users = target_users.exclude(id__in=already_swape_list)[:20]
     return target_users
+
+
+def recommand_users(uid):
+    ''' Algorithm of recommanding users'''
+    users_que = set(recommand_from_queue(uid))
+    remain_nums = 20 - len(users_que)
+    if remain_nums > 0:
+        users_db = set(recommand_from_db(uid,remain_nums))
+        users = users_que | users_db
+    return users
 
 
 def like_someone(uid, sid):
@@ -40,6 +58,8 @@ def like_someone(uid, sid):
         return False
     else:
         swipe.save()
+        myKey = RCMD_QUE % uid
+        rds.lrem(myKey,0,sid)
     # TODO: 2. check he/she like/super login-User or not, if it is true, then they are friends
     if Swiped.is_liked(sid, uid):
         Friend.make_friends(uid, sid)
@@ -60,6 +80,8 @@ def superlike_someone(uid, sid):
         return False
     else:
         swipe.save()
+        myKey = RCMD_QUE % uid
+        rds.lrem(myKey, 0, sid)
     # TODO: 2.1 check he/she like/super login-User or not, if it is true, then they are friends.
     # TODO: 2.2 if he/she is not dislike you, then add he/she to his/her recommand-list.
     liked = Swiped.is_liked(sid, uid)
@@ -70,5 +92,5 @@ def superlike_someone(uid, sid):
         return False
     else:
         he_or_she_recommand_key = RCMD_QUE % sid
-        rds.lpush(he_or_she_recommand_key,uid)
+        rds.rpush(he_or_she_recommand_key, uid)
         return False
